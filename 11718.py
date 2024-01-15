ad87d5f42ce896061eb865bbc154fe3a2a27a3ed
choeyunseo <choeyunseo4060@naver.com>
2024-01-16 01:31:15 +0900

    Add 11718.py and 5622.py

--- 11718.py
+++ 11718.py
@@ -1,7 +1,11 @@
 # 그대로 출력하기
 while True:
-    string = input()
-    if string == None:
+    try:
+        s = input()
+        if s[0] == " " or s[-1] == " ":
+            break
+        else:
+            print(s)
+    # 예외 (더 이상 읽을 내용이 없을 때)
+    except EOFError:
         break
-    else:
-        print(string)
