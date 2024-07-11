9a02ef95d7eb93f6ff445b3b66ed39bef9a0afe7
choeyunseo <choeyunseo4060@naver.com>
2024-07-11 23:40:00 +0900

    Update 11724 Success

--- graphTraversal/11724.py
+++ graphTraversal/11724.py
@@ -1,10 +1,13 @@
 # 연결 요소의 개수
-n, m = list(map(int, input().split())) # 정점, 간선 개수
+import sys
+sys.setrecursionlimit(10000)
+
+n, m = list(map(int, sys.stdin.readline().split())) # 정점, 간선 개수
 
 adj_mat = [[0] * (n) for _ in range(n)]
 
 for _ in range(m):
-    u, v = list(map(int, input().split()))
+    u, v = list(map(int, sys.stdin.readline().split()))
     adj_mat[u - 1][v - 1] = 1
     adj_mat[v - 1][u - 1] = 1
 
@@ -23,7 +26,6 @@ def dfs(start):
 cnt = 0
 for i in range(n):
     if i not in visited:
-        # print(f'vertex: {i}')
         dfs(i)
         cnt += 1
 
