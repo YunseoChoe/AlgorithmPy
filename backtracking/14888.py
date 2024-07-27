c24a3e235a3c029f5a2afb4214ad4a3308f154af
choeyunseo <choeyunseo4060@naver.com>
2024-07-27 20:37:34 +0900

    Reduce 14888

--- backtracking/14888.py
+++ backtracking/14888.py
@@ -1,7 +1,7 @@
+# 연산자 끼워넣기
 import copy
 from itertools import permutations
 
-# 연산자 끼워넣기
 n = int(input())
 num = map(int, input().split())
 num = list(num)
@@ -14,7 +14,7 @@ for i in range(len(operator)):
     for j in range(operator[i]):
         op.append(operators[i])
 
-p = [] # 연산자 순열 배열
+# p = [] # 연산자 순열 배열
 
 # 순열 재귀 함수 (DFS)
 # arr = []
@@ -39,42 +39,26 @@ p = [] # 연산자 순열 배열
 
 list_p = list(set(permutations(op, n - 1))) # 중복 순열 제거
 results = [] # 각 계산 결과를 저장하는 배열
-sum_list = [] # 각 sum을 저장하는 배열
 
-# 각 순열에 대한 조합 찾기
-for i in range(len(list_p)):
-    # calculate 배열 초기화
-    calculate = [0] * (len(num) + len(op))
-    odd = 0 # num 인덱스
-    even = 0 # p 인덱스
-    for j in range(len(calculate)):
-        # j가 짝수면 숫자
-        if j % 2 == 0:
-            calculate[j] = num[odd]
-            odd += 1
-        # j가 홀수면 연산자
-        else:
-            calculate[j] = list_p[i][even]
-            even += 1
-    results.append(calculate) 
-
-    # sum 구하기
-    sum = calculate[0]
-    for j in range(1, len(calculate) - 1):
+while len(list_p) != 0:
+    operator = list_p.pop()
+    # 합 구하기
+    sum = num[0]
+    for i in range(1, len(num)):
         # 더하기
-        if calculate[j] == '+':
-            sum += calculate[j + 1]
+        if operator[i - 1] == '+':
+            sum += num[i]
         # 빼기
-        elif calculate[j] == '-':
-            sum -= calculate[j + 1]
+        elif operator[i - 1] == '-':
+            sum -= num[i]
         # 곱하기
-        elif calculate[j] == '*':
-            sum *= calculate[j + 1]
+        elif operator[i - 1] == '*':
+            sum *= num[i]
         # 나누기
-        elif calculate[j] == '/':
-            sum = int(sum / calculate[j + 1])
+        else:
+            sum = int(sum / num[i])
+    
+    results.append(sum)
 
-    sum_list.append(sum)
-
-print(max(sum_list))
-print(min(sum_list))
+print(max(results))
+print(min(results))
