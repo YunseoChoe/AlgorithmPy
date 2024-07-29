ccf0faf4e74f231e05fb8a29236c965a7ec66209
choeyunseo <choeyunseo4060@naver.com>
2024-07-29 17:53:11 +0900

    Add 6603 another way

--- backtracking/6603.py
+++ backtracking/6603.py
@@ -1,23 +1,49 @@
 # 로또
-arr = []
-def func(start):
-    if len(arr) == 6:
-        for i in range(len(arr)):
-            print(s[arr[i]], end = " ")
-        print()
-        return
+# arr = []
+# def func(start):
+#     if len(arr) == 6:
+#         for i in range(len(arr)):
+#             print(s[arr[i]], end = " ")
+#         print()
+#         return
     
-    for i in range(start, len(s)):
-        if i not in arr:
-            arr.append(i)
-            func(i + 1)
-            arr.pop()
+#     for i in range(start, len(s)):
+#         if i not in arr:
+#             arr.append(i)
+#             func(i + 1)
+#             arr.pop()
 
+# while True:
+#     num = list(map(int, input().split()))
+#     if num[0] == 0:
+#         break
+#     s = num[1:]
+
+#     func(0)
+#     print()
+
+
+# 로또 복습
 while True:
     num = list(map(int, input().split()))
     if num[0] == 0:
         break
+
     s = num[1:]
+
+    arr = []
+    def func(start):
+        if len(arr) == 6:
+            for i in range(len(arr)):
+                print(arr[i], end = " ")
+            print()
+            return
+        else:
+            for i in range(start, len(s)):
+                if s[i] not in arr:
+                    arr.append(s[i])
+                    func(i + 1)
+                    arr.pop()
 
     func(0)
     print()
