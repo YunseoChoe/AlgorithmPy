083c221a7016251192b8de0ce40c16a345a0210a
choeyunseo <choeyunseo4060@naver.com>
2024-08-14 23:40:20 +0900

    Add 1759

--- backtracking/1759.py
+++ backtracking/1759.py
@@ -1,16 +1,17 @@
 # 암호 만들기
-# 순열 말고 조합, 왜냐하면 어차피 정렬하면 같은 값이기 때문, 재귀 조정 맘대로 해도 됨?
+import copy
+
 l, c = list(map(int, input().split()))
 chrs = list(input().split(' '))
 
-# 정렬 되어있는지 and 최소 한 개의 모음 & 최소 두 개의 자음인지
+# 정렬 되어있는지, 최소 한 개의 모음과 최소 두 개의 자음인지
 def check(arr):
     vowel = ['a', 'e', 'i', 'o', 'u'] # 모음
     consonant = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'z'] # 자음
     is_in_vowel = False 
     is_in_consonant = False
     is_consonant = 0
-
+    
     for i in range(len(arr)):
         if arr[i] in vowel:
             is_in_vowel = True
@@ -18,35 +19,41 @@ def check(arr):
             is_consonant += 1
         if is_consonant >= 2:
             is_in_consonant = True
-    
+
     if is_in_consonant and is_in_vowel:
         return True
     else:
         return False
 
 arr = []
+all_arr = []
 def func():
+    global all_arr
     if len(arr) == l:
         # 조건 비교
         if check(arr):
-            # 출력
-            for i in range(len(arr)):
-                print(arr[i], end = "")
-            print()
+            # 배열 저장
+            all_arr.append(copy.deepcopy(arr))
+            # print(arr)
             return
         
     for i in range(len(chrs)):
-        # arr가 비어있을 경우
+        # arr가 비었을 경우
         if len(arr) == 0:
             arr.append(chrs[i])
             func()
             arr.pop()
-
-        # arr가 안 비어있을 경우
         else:
-            if arr[-1] < chrs[i]: # 정렬
+            if arr[-1] < chrs[i]:
                 arr.append(chrs[i])
                 func()
                 arr.pop()
 
 func()
+
+# 출력
+all_arr.sort() # 사전순 정렬
+for i in range(len(all_arr)):
+    for j in range(len(all_arr[i])):
+        print(all_arr[i][j], end = "")
+    print()
