055ed3f9fadfdf3a64d10d09ecd5b7fde4786cf0
choeyunseo <choeyunseo4060@naver.com>
2024-08-25 15:43:04 +0900

    Add 2630

---
+++ recursion/2630.py
@@ -0,0 +1,41 @@
+# 색종이 만들기
+n = int(input())
+board = []
+white_cnt = 0
+blue_cnt = 0
+
+for i in range(n):
+    color = list(map(int, input().split()))
+    board.append(color)
+
+def func(x, y, n):
+    global white_cnt, blue_cnt
+    color = board[x][y]
+    is_same = True
+    
+    # 색상 검사
+    for i in range(x, x + n):
+        for j in range(y, y + n):
+            if color != board[i][j]:
+                is_same = False
+
+    # 모든 색상이 같지 않다면 
+    if is_same == False:
+        func(x, y, n // 2)                      # 1사분면
+        func(x, y + n // 2, n // 2)             # 2사분면
+        func(x + n // 2, y, n // 2)             # 3사분면
+        func(x + n // 2, y + n // 2, n // 2)    # 4사분면
+
+    # 모든 색상이 같다면 cnt값 + 1
+    else:
+        # 흰색이라면
+        if color == 0:
+            white_cnt += 1
+        # 검은색이라면
+        else:
+            blue_cnt += 1
+
+func(0, 0, n)
+
+print(white_cnt)
+print(blue_cnt)
