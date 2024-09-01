db2fcf2ce4c434e3d5a5d7e594700cf7d9f66db8
choeyunseo <choeyunseo4060@naver.com>
2024-09-02 01:05:19 +0900

    Update 2178

--- graphTraversal/2178.py
+++ graphTraversal/2178.py
@@ -1,42 +1,30 @@
 # 미로 탐색
-n, m = input().split()
-n = int(n)
-m = int(m)
-
+n, m = list(map(int, input().split()))
 miro = []
+
 for i in range(n):
     num = list(input())
     for j in range(len(num)):
         num[j] = int(num[j])
     miro.append(num)
 
-dx = [-1, 1, 0, 0] # 상하좌우 좌표
+dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-def bfs(start): # 시작 좌표
-    queue = []
-    queue.append(start) 
-
-    while not len(queue) == 0:
-        x, y = queue.pop(0)
+queue = []
+def bfs(start_x, start_y):
+    queue.append((start_x, start_y))
+ 
+    while len(queue) != 0:
+        start_x, start_y = queue.pop(0)
         for i in range(4):
-            x_1 = x + dx[i]
-            y_1 = y + dy[i]
-         
-            if 0 <= x_1 < n and 0 <= y_1 < m and miro[x_1][y_1] == 1: 
-                # if dx[i] == -1 and dy[i] == 0:
-                #     print("상")
-                # elif dx[i] == 1 and dy[i] == 0:
-                #     print("하")
-                # elif dx[i] == 0 and dy[i] == -1:
-                #     print("좌")
-                # elif dx[i] == 0 and dy[i] == 1:
-                #     print("우")
-                
-                miro[x_1][y_1] = miro[x][y] + 1
-                queue.append((x_1, y_1))
-    
-    return miro[n - 1][m - 1]
+            x = start_x + dx[i]
+            y = start_y + dy[i]
 
-return_value = bfs((0,0))
-print(return_value)
+            if 0 <= x < n and 0 <= y < m and miro[x][y] == 1:
+                miro[x][y] = miro[start_x][start_y] + 1
+                queue.append((x, y))
+
+    return miro[n - 1][m - 1]
+    
+print(bfs(0, 0))
