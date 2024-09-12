ac162af9d5b62d91d522e9008f8eedf4b60368ba
choeyunseo <choeyunseo4060@naver.com>
2024-09-12 22:10:29 +0900

    Add 2644

--- graphTraversal/2644.py
+++ graphTraversal/2644.py
@@ -1,11 +1,14 @@
 # 촌수계산
 n = int(input())
 adj_mat = []
+flag = False
 for i in range(n + 1):
     mat = []
     for j in range(n + 1):
         mat.append(0)
     adj_mat.append(mat)
+
+visited = [False] * (n + 1)
 
 a, b = list(map(int, input().split()))
 m = int(input())
@@ -14,23 +17,19 @@ for i in range(m):
     adj_mat[x][y] = 1
     adj_mat[y][x] = 1
 
-visited = [0] * (n + 1)
-
 def dfs(start, count):
-    global flag
-    if visited[start] == 1:
+    global flag 
+    if visited[start]:
         return
     if start == b:
         flag = True
         print(count)
-        exit()
-    visited[start] = 1
-    for i in range(len(adj_mat)):
-        if adj_mat[start][i] == 1 and visited[i] == 0:
+        exit(0)
+    visited[start] = True
+    for i in range(n + 1):
+        if visited[i] == False and adj_mat[start][i] == 1:
             dfs(i, count + 1)
-
-flag = False
+    
 dfs(a, 0)
-
-if flag == False:
+if not flag:
     print(-1)
