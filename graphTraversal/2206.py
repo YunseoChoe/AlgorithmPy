6eb0fc8cc6c0662e8453659f39ed293bfe13d0cc
choeyunseo <choeyunseo4060@naver.com>
2025-01-09 23:24:35 +0900

    Update 2206

--- graphTraversal/2206.py
+++ graphTraversal/2206.py
@@ -10,43 +10,36 @@ dy = [1, -1, 0, 0]
 def bfs(start_x, start_y):
     q = deque()
     visited[start_x][start_y][0] = 1 # 시작점 경로는 1
-    q.append((start_x, start_y))
+    q.append((start_x, start_y, 0))
 
     while q:
-        new_x, new_y = q.popleft()
+        new_x, new_y, b = q.popleft()
         # 목적지에 도달했다면
         if new_x + 1 == n and new_y + 1 == m:
-            print(max(visited[new_x][new_y][0], visited[new_x][new_y][1]))
+            print(visited[new_x][new_y][b])
             return
         
         # 동서남북
         for i in range(4):
             nx = new_x + dx[i]
             ny = new_y + dy[i]
-
             if 0 <= nx < n and 0 <= ny < m:
-                # 0이라면
+                # 벽이 아니라면
                 if graph[nx][ny] == "0":
-                    # 벽을 1번 부셨고, 방문하지 않았다면
-                    if visited[new_x][new_y][1] > 0 and visited[new_x][new_y][0] == 0:
-                        visited[nx][ny][1] = visited[new_x][new_y][1] + 1
-                        q.append((nx, ny))
-                    # 벽을 부수지 않았고, 방문하지 않았다면
-                    elif visited[new_x][new_y][1] == 0 and visited[nx][ny][0] == 0:
-                        visited[nx][ny][0] = visited[new_x][new_y][0] + 1
-                        q.append((nx, ny))
-                
-                # 1이고, 방문하지 않았다면
-                if graph[nx][ny] == "1" and visited[nx][ny][0] == 0 and visited[nx][ny][1] == 0:
-                    # 벽을 부수고 오지 않았다면
-                    if visited[new_x][new_y][1] == 0:
-                        visited[nx][ny][1] = visited[new_x][new_y][0] + 1
-                        q.append((nx, ny))
+                    # 방문하지 않았다면
+                    if visited[nx][ny][b] == 0: 
+                        q.append((nx, ny, b))
+                        visited[nx][ny][b] = visited[new_x][new_y][b] + 1
+
+                # 벽이라면
+                elif graph[nx][ny] == "1" and b == 0:
+                    # 벽을 부시지 않았고, 방문하지 않았다면
+                    visited[nx][ny][1] = visited[new_x][new_y][0] + 1
+                    q.append((nx, ny, 1))
 
     # 목적지에 도달하지 못했다면 -1 출력
     print(-1)
     return
-
 
 if __name__ == '__main__':
     # 입력 받기
@@ -67,9 +60,3 @@ if __name__ == '__main__':
 
     # (0, 0)부터 bfs 시작
     bfs(0, 0)
-
-
-
-
-
-
