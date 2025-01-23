2f45c4bec366b3c4636641d9e4b508e25eaaa177
choeyunseo <choeyunseo4060@naver.com>
2025-01-23 14:50:05 +0900

    Add 6593

--- graphTraversal/6593.py
+++ graphTraversal/6593.py
@@ -11,16 +11,13 @@ dz = [0, 0, 0, 0, -1, 1]
 print_list = [] # 출력 배열
 
 def bfs(x, y, z, dest_x, dest_y, dest_z):
-    print(f'목적지 좌표: {dest_z}층 {dest_x}행 {dest_y}열')
     q = deque()
-    q.append((z, x, y))
+    q.append((x, y, z))
     while q:
-        new_z, new_x, new_y = q.popleft()
-        print(new_z, new_x, new_y)
+        # print(f'q 내용: {q}')
+        new_x, new_y, new_z = q.popleft()
         # 목적지에 도착했으면 종료
         if new_x == dest_x and new_y == dest_y and new_z == dest_z:
-            print("목적지 도착!")
-            print_list.append()
             p = f'Escaped in {visited[dest_z][dest_x][dest_y]} minute(s).'
             print_list.append(p)
             return
@@ -31,14 +28,13 @@ def bfs(x, y, z, dest_x, dest_y, dest_z):
             nz = new_z + dz[i]
             if 0 <= nx < r and 0 <= ny < c and 0 <= nz < l:
                 # print(f"현재 보고있는 좌표값: {nz}층 {nx}행 {ny}열")
-                # 방문하지 않았고, 비어있는 칸이면 이동
-                if visited[nz][nx][ny] == 0 and graph[nz][nx][ny] == ".":
-                    q.append((nz, nx, ny))
+                # 방문하지 않았고, 비어있는 칸 또는 목적지라면 이동
+                if visited[nz][nx][ny] == 0 and (graph[nz][nx][ny] == "." or graph[nz][nx][ny] == "E"):
+                    q.append((nx, ny, nz))
                     # 방문 배열에 경로를 누적
                     visited[nz][nx][ny] = visited[new_z][new_x][new_y] + 1
-                    print(f'visited 출력: {visited}')
-    w = "Trapped!"
-    print_list.append(w)
+                
+    print_list.append("Trapped!")
            
 if __name__ == '__main__':
     while True:
@@ -64,25 +60,21 @@ if __name__ == '__main__':
                 graph_list = input().strip()
                 graph_mat.append(graph_list)
             graph.append(graph_mat)
-        print(f'graph 출력: {graph}')
+            line = input()
+        
 
         # 시작점, 목적지 찾기
         for i in range(l):
             for j in range(r):
                 for k in range(c):
                     if graph[i][j][k] == "S":
-                        start_z = i
-                        start_x = j
-                        start_y = k
+                        start_z, start_x, start_y = i, j, k
                     elif graph[i][j][k] == "E":
-                        dest_z = i
-                        dest_x = j
-                        dest_y = k
+                        dest_z, dest_x, dest_y = i, j, k
 
         # bfs호출
-        bfs(start_z, start_x, start_y, dest_x, dest_y, dest_z)
+        bfs(start_x, start_y, start_z, dest_x, dest_y, dest_z)
 
-        # 최종 출력
-        for i in range(len(print_list)):
-            print(print_list[i])
-
+    # 최종 출력
+    for i in range(len(print_list)):
+        print(print_list[i])
