eef87a538a7dea39865f42a0f66b29eb314962e7
choeyunseo <choeyunseo4060@naver.com>
2025-02-07 00:29:14 +0900

    Update 16933 시간초과

--- graphTraversal/16933.py
+++ graphTraversal/16933.py
@@ -7,19 +7,15 @@ input = sys.stdin.readline
 dx = [0, 0, 1, -1]
 dy = [1, -1, 0, 0]
 
-def bfs(start_x, start_y):
+def bfs():
     q = deque()
-    q.append((start_x, start_y, 0)) # x, y, breaks (벽을 부수지 않고 시작)
-    visited[start_x][start_y][0] = 1 # 시작점부터 경로는 1
-    day = True # 낮부터 시작
-
+    q.append((0, 0, 0, 1)) 
+    visited[0][0][0] = 1
+    
     while q:
-        cur_x, cur_y, breaks = q.popleft()
-        print(f'현재 보고있는 cur_x, cur_y: {cur_x}, {cur_y}')
-        
+        cur_x, cur_y, breaks, dist = q.popleft()
         # 도착했다면 경로 출력 후 종료
         if cur_x == (n - 1) and cur_y == (m - 1):
-            print("도착!")
             print(visited[cur_x][cur_y][breaks]) 
             return
 
@@ -28,43 +24,20 @@ def bfs(start_x, start_y):
             nx = cur_x + dx[i]
             ny = cur_y + dy[i]
             if 0 <= nx < n and 0 <= ny < m:
-                print(f'nx, ny: {nx}, {ny}')
-                # 벽이 아닐 때 (day 상관 x)
+                # 벽이 아닐 때
                 if visited[nx][ny][breaks] == 0 and graph[nx][ny] == 0:
-                    print("벽이 아님")
-                    q.append((nx, ny, breaks))
-                    visited[nx][ny][breaks] = visited[cur_x][cur_y][breaks] + 1
-                    print(f'방문배열: {visited}')
-                    # 밤, 낮 변경
-                    if day == False:
-                        day = True
-                    else:
-                        day = False
-
-                # 벽일 때 (day가 True일 때만 이동)
-                if graph[nx][ny] == 1:
-                    print("벽임")
-                    # 밤이면 제자리 이동
-                    if day == False:
-                        print("밤")
-                        q.append((cur_x, cur_y, breaks))
-                        visited[cur_x][cur_y][breaks] = visited[cur_x][cur_y][breaks] + 1
-                        print(f'방문배열: {visited}')
-                        # 낮으로 변경
-                        day = True
+                    q.append((nx, ny, breaks, dist + 1))
+                    visited[nx][ny][breaks] = dist + 1
                     
+                # 벽일 때 
+                if graph[nx][ny] == 1:
+                    # 밤이면 제자리 이동
+                    if dist % 2 == 0:
+                        q.append((cur_x, cur_y, breaks, dist + 1))
                     # 낮이면 그냥 이동
-                    else:
-                        print("낮")
-                        # 벽을 (k - 1)번 이하로 부셨다면 그냥 이동 (0번도 포함)
-                        if breaks < k and visited[nx][ny][breaks + 1] == 0:
-                            print("벽을 k - 1번 이하로 부심")
-                            q.append((nx, ny, breaks + 1))
-                            visited[nx][ny][breaks + 1] = visited[cur_x][cur_y][breaks] + 1
-                            print(f'방문배열: {visited}')
-                            # 밤으로 변경
-                            day = False
-
+                    elif dist % 2 == 1 and breaks < k and visited[nx][ny][breaks + 1] == 0:
+                        q.append((nx, ny, breaks + 1, dist + 1))
+                        visited[nx][ny][breaks + 1] = dist + 1
     # 도달하지 못했을 경우
     print(-1)
     return
@@ -87,10 +60,5 @@ if __name__ == '__main__':
         visited.append(visited_mat)
 
     # (0, 0)부터 bfs 시작
-    bfs(0, 0)
-    print(visited)
-
-
-
-
-
+    bfs() # 거리는 1부터 시작
+    
