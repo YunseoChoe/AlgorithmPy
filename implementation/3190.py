c07966cf3e106d52af666ead603965be3e6d2cf9
choeyunseo <choeyunseo4060@naver.com>
2025-02-07 16:51:00 +0900

    Update 3190 except ex2

--- implementation/3190.py
+++ implementation/3190.py
@@ -1,23 +1,29 @@
 # 뱀
-if __name__ == '__main__':
-    # 동서남북 
-    dx = [0, 0, 1, -1]
-    dy = [1, -1, 0, 0]
+import sys
+from collections import deque
+input = sys.stdin.readline
 
+if __name__ == '__main__':
+    # 동남서북
+    dx = [0, 1, 0, -1]
+    dy = [1, 0, -1, 0] 
+    
+
+    q = deque()
     n = int(input()) # 보드의 크기
     k = int(input()) # 사과의 개수
+
     # 보드 초기화
     graph = [] 
     for _ in range(n):
         graph_list = [0] * n
         graph.append(graph_list)
-
-    print(graph)
     
     # 사과 입력받기
     for _ in range(k):
         apple_x, apple_y = map(int, input().split())
         graph[apple_x - 1][apple_y - 1] = 1 # 사과는 1
+
 
     # 뱀의 방향 횟수 입력받기
     snake_ways = {}
@@ -26,56 +32,47 @@ if __name__ == '__main__':
         second, way = input().split()
         snake_ways[second] = way
 
-    print(snake_ways)
-
     total_second = 1
-    head_x = 0 # 뱀은 (0, 0)에서 시작하고 오른쪽(1)을 바라보고 있음
-    head_y = 0
-    tail_x = 0
-    tail_y = 0
-    d = 1 
+    d = 0 # 뱀은 동쪽을 보고 시작
+    nx = 0
+    ny = 0
+    q.append((nx, ny)) # 뱀의 첫 위치 넣기
 
     while True:
         total_second += 1
-        # 종료조건
-        if graph[head_x][head_y] == 2 or head_x < 0 or head_y < 0:
-            print("게임 종료")  
-            break
 
         # 방향 전환
         for s in snake_ways.keys(): 
             # 만약 해당 초가 됐다면
-            if int(s) == total_second:
+            if int(s) == total_second - 1:
                 # 방향이 L이라면 왼쪽으로 90도 회전
                 if snake_ways[s] == 'L':
-                    pass
+                    d = (d + 3) % 4
                 # 방향이 D라면 오른쪽으로 90도 회전
                 elif snake_ways[s] == 'D':
-                    pass
+                    d = (d + 1) % 4
 
+        # 이동할 좌표 구하기 (d에 따라서)
+        nx = nx + dx[d]
+        ny = ny + dy[d]
+
+        # 종료 조건
+        # 벽이거나 자기 자신이면 종료
+        if not (0 <= nx < n) or not (0 <= ny < n) or (nx, ny) in q:
+            print(total_second)
+            break
 
         # 사과가 있다면 (머리는 이동, 꼬리는 이동 x)
         if graph[nx][ny] == 1:
-            graph[head_x][head_y] == 0 # 현재 칸에 사과 없애기
+            # 현재 칸 사과 없애기
+            graph[nx][ny] = 0
             # 뱀의 머리만 이동
-            head_x = nx 
-            head_y = ny
+            q.append((nx, ny))
             
-        # 사과가 없다면 (머리와 꼬리 모두 이동, 크기는 변하지 않음)
+        # 사과가 없다면 
         else:
-            tail_x = ??
-            tail_y = ??
-            head_x = nx
-            head_y = ny
-            
-
-        
-
-
-
-        
-
-                
-                
-
+            # 머리 이동
+            q.append((nx, ny))
+            # 꼬리 이동
+            q.popleft()
 
