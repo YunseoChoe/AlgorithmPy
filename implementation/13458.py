8dbf4ed8b90b675a4b8a01d2220fa0a2a326aa99
choeyunseo <choeyunseo4060@naver.com>
2025-02-17 14:25:18 +0900

    Add 13458

--- implementation/13458.py
+++ implementation/13458.py
@@ -1,17 +1,21 @@
-n = int(input())
-s = list(map(int, input().split()))
-b, c = list(map(int, input().split()))
+# 시험 감독
+if __name__ == '__main__':
+    n = int(input())
+    s = list(map(int, input().split()))
+    b, c = list(map(int, input().split()))
 
-main_d = 0
-sub_d = 0
+    main_d = 0
+    sub_d = 0
 
-for i in range(n):
-    s[i] = s[i] - b
-    main_d += 1
-    if s[i] >= 0:
-        if s[i] % c == 0:
-            sub_d = s[i] // c 
-        else:
-            sub_d = s[i] // c + 1
+    for i in range(n):
+        # 1. 총감독관
+        s[i] = s[i] - b
+        main_d += 1
+        # 2. 부감독관
+        if s[i] >= 0:
+            if s[i] % c == 0:
+                sub_d += s[i] // c 
+            else:
+                sub_d += s[i] // c + 1
 
-print(main_d + sub_d)
+    print(main_d + sub_d)
