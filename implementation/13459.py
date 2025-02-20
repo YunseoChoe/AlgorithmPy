a597f4ea0707940be875b4c1cf3f2ffa6b52d2e0
choeyunseo <choeyunseo4060@naver.com>
2025-02-20 22:54:10 +0900

    Update 13459 틀만

--- implementation/13459.py
+++ implementation/13459.py
@@ -6,57 +6,16 @@ dx = [0, 0, 1, -1]
 dy = [1, -1, 0, 0]
 
 # 구슬 움직이는 함수 
-def move(x, y, dx, dy):
-    is_hall = False
-    # 동
-    if dx == 0 and dy == 1:
-        wall_distance = (m - 1) - y
-        while True:
-            # 만약 벽이 아니면 이동
-            if graph[x][y + 1] == '#':
-                break
-            # 만약 구멍이면
-            elif graph[x][y + 1] == 'O':
-                is_hall = True
-                y += 1
-                break
-            y += 1 # 이동
-            
-    # 서
-    elif dx == 0 and dy == (-1):
-        wall_distance = y
-        while True:
-            if graph[x][y - 1] == '#':
-                break
-            elif graph[x][y - 1] == 'O':
-                is_hall = True
-                y -= 1
-                break
-            y -= 1
-    # 남
-    elif dx == 1 and dy == 0:
-        wall_distance = (n - 1) - x
-        while True:
-            if graph[x + 1][y] == '#':
-                break
-            elif graph[x + 1][y] == 'O':
-                is_hall = True
-                x += 1
-                break
-            x += 1
-    # 북
-    elif dx == (-1) and dy == 0:
-        wall_distance = x
-        while True:
-            if graph[x - 1][y] == '#':
-                break
-            elif graph[x - 1][y] == 'O':
-                is_hall = True
-                x -= 1
-                break
-            x -= 1
+def move(x, y, dir):
+    nx, ny = x, y
+    cnt = 0 # 벽과의 거리 저장
 
-    return x, y, wall_distance, is_hall
+    # 구멍, 벽이 아닐 때까지 이동
+    while graph[nx][ny] != 'O' and graph[nx][ny] == '#':
+        nx += dx[dir]
+        ny += dy[dir]
+        cnt += 1
+    return nx, ny, cnt
 
 def bfs(start_red_x, start_red_y, start_blue_x, start_blue_y):
     q = deque()
@@ -71,52 +30,24 @@ def bfs(start_red_x, start_red_y, start_blue_x, start_blue_y):
         # 구슬 이동
         for i in range(4):
             # 새로운 x, 새로운 y, 벽과의 거리
-            red_nx, red_ny, red_distance, red_is_hall = move(red_x, red_y, dx[i], dy[i]) 
-            blue_nx, blue_ny, blue_distance, blue_is_hall = move(blue_x, blue_y, dx[i], dy[i])
+            rnx, rny, rcnt = move(red_x, red_y, dx[i], dy[i]) 
+            bnx, bny, bcnt = move(blue_x, blue_y, dx[i], dy[i])
 
-            # 파란색이 구멍에 들어갔을 경우 종료
-            if blue_is_hall:
-                print(0)
-                break 
-
-            # 빨간색이 구멍에 들어갔을 경우
-            if red_is_hall:
-                print(1)
-                break
-
-            # 빨간색이 구멍에 들어가지 않았을 경우 
-            else:
-                # 만약 red랑 blue가 같은 위치에 있다면 이동 금지
-                if red_nx == blue_nx and red_ny == blue_ny:
-                    # 빨간공이 더 벽과 가깝다면
-                    if red_distance < blue_distance:
-                        # 동
-                        if dx[i] == 0 and dy[i] == 1:
-                            blue_ny -= 1
-                        # 서
-                        elif dx[i] == 0 and dy[i] == -1:
-                            blue_ny += 1
-                        # 남
-                        elif dx[i] == 1 and dy[i] == 0:
-                            blue_nx -= 1
-                        # 북
-                        else:
-                            blue_nx += 1
-                    # 파란공이 더 벽과 가깝다면
-                    else:
-                        # 동
-                        if dx[i] == 0 and dy[i] == 1:
-                            red_ny -= 1
-                        # 서
-                        elif dx[i] == 0 and dy[i] == -1:
-                            red_ny += 1
-                        # 남
-                        elif dx[i] == 1 and dy[i] == 0:
-                            red_nx -= 1
-                        # 북
-                        else:
-                            red_nx += 1
-                q.append((red_nx, red_ny, blue_nx, blue_ny, count + 1))
+            # 파란공이 구멍에 안 들어갔을 경우
+            if graph[bnx][bny] != 'O':
+                # 빨간공이 구멍에 들어갔을 경우
+                if graph[rnx][rny] == 'O':
+                    return True
+                # 빨간공과 파란공 위치가 같을 경우
+                if rnx == bnx and rny == bny:
+                    # cnt를 기준으로 재배치 
+                    pass
+                # 만약 방문하지 않았다면 
+                if not visited[rnx][rny][bnx][bny] == False:
+                    # 방문처리
+                    visited[rnx][rny][bnx][bny] == True
+                    # Q에 넣기
+                    q.append((rnx, rny, bnx, bny, count + 1))
 
 if __name__ == '__main__':
     n, m = map(int, input().split())
@@ -125,6 +56,7 @@ if __name__ == '__main__':
         graph_list = list(input())
         graph.append(graph_list)
 
+    # 방문배열 초기화 (4차원 or 1차원)
     visited = [] 
 
     # 시작 red, blue 위치 찾기
@@ -136,6 +68,5 @@ if __name__ == '__main__':
             elif graph[i][j] == 'R':
                 start_red_x = i
                 start_red_y = j
-
-
+    
     bfs(start_red_x, start_red_y, start_blue_x, start_blue_y)
